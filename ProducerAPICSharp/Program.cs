8249813f9952f679b29fa3687ae68d833537c52c
SanjaySolankiUS <150522812+SanjaySolankiUS@users.noreply.github.com>
2023-12-06 12:10:39 +0530

    Initial Working Draft
    -- Install .Net 8.0 SDK
    -- Install nuget.exe
    
    
    
    dotnet new console --framework net8.0 --use-program-main
    
    dotnet add package Confluent.Kafka --version 2.3.0
    
    dotnet run

---
+++ ProducerAPICSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ProducerAPICSharp;
+using Confluent.Kafka;
+using System;
+//using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+            Console.WriteLine("Hello, World!");
+        ////
+            var config = new ProducerConfig {
+                        BootstrapServers = "localhost:9092"                        
+                        };
+
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+
+                for(int i=0; i < 10; i++)
+                {
+                    Console.WriteLine("Hey, How are you " + i);
+                    producer.ProduceAsync("ClassAtt", new Message<string, string>(){                        
+                        Value = $"Hey, How are you " + i
+                    }).Wait();
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10)); 
+            }
+
+
+
+            Console.Read();
+    }
+}
